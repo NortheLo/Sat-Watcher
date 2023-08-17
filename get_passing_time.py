675f61211b67d25a439226c6cea9bc33dd580213
NortheLo <19312092+NortheLo@users.noreply.github.com>
2023-08-18 01:37:25 +0200

    Added API communication; Added json parser
    Bug: Doenst find key in json/dict

--- get_passing_time.py
+++ get_passing_time.py
@@ -1,10 +1,30 @@
-import requests
+from n2yo import n2yo
 import json
 
-api_url = "https://api.n2yo.com/rest/v1/satellite/"
+api_key = ""
+lat = 0.0
+long = 0.0
+alt = 0
+
+METEOR_M2_3_ID = 57166
 
 def main():
-    print("Hello")
+    session = n2yo.N2YO(api_key, lat, long, alt)
+    passes = session.get_radio_passes(METEOR_M2_3_ID, 3, lat, long, alt)
+
+    print(passes)
+    print(len(passes))
+    print("\n\n")
+
+    for i in range(0, len(passes)):
+        str_passes = json.dumps(passes[i])
+        json_passes = json.loads(str_passes)
+        
+        # Error: Doesnt find the key when present
+        if 'startUCT' in json_passes:
+            print("Time found!")
+        else:
+            print("No time found!")
 
 if __name__ == "__main__":
     main()
