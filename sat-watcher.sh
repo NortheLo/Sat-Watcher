91775dd5a4f50400d07f7a88ed0d614d2cf027d8
NortheLo <19312092+NortheLo@users.noreply.github.com>
2023-08-18 01:34:38 +0200

    Added cron setup routine

--- sat-watcher.sh
+++ sat-watcher.sh
@@ -37,6 +37,17 @@ function build_satdump() {
 	sudo make install
 }
 
+function setup_cron() {
+	local cron_cmd="0 12 * * * /bin/usr/python3 /home/$USER/sat-watcher/get_passing_time.py"
+
+	if grep -q get_passing_time.py /etc/crontab; then
+		echo "WARNING: crontab is already configured!"
+	else 
+		echo "INFO: Adding crontab configuration"
+		echo "$cron_cmd" | sudo tee -a /etc/crontab
+	fi
+}
+
 function help() {
 	echo "This is a helper script to automate the build process of satdump and other tools for receiving satelite images"
 	echo "To build for an Red Hat based system type: ./sat-script.sh install_rh"
